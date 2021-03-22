9296dcd76ba84fafaf50c8a1a2418a9846be64e7
Frederick Dodd <fd6@sanger.ac.uk>
2021-03-22 14:30:25 +0000

    Moving before_install conda into before install script

--- .travis/install.sh
+++ .travis/install.sh
@@ -12,9 +12,37 @@ CONDA_TEST_ENV=$3
 
 TRAVIS_PYTHON_VERSION=$4
 
+CONDA_CHANNEL=$5
+
+# testing adding conda stuff here TODO
+wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-4.5.11-Linux-x86_64.sh -O miniconda.sh
+
+/bin/bash miniconda.sh -b -p "$HOME/miniconda"
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+
+conda config --set always_yes yes
+conda config --set changeps1 no
+conda config --set show_channel_urls true
+#conda update -q conda # temp remove TODO
+
+#check conda environments
+conda info --envs
+#
+
+conda config --add channels $CONDA_CHANNEL
+
+# Useful for debugging any issues with conda
+conda info -a
+
+##### TODO
+
+
+
 #TODO move these to conda section?
 export PATH="$HOME/miniconda/bin:$PATH"
 source activate "$CONDA_TEST_ENV"
+#
 
 # CPAN as in npg_npg_deploy
 cpanm --notest --reinstall App::cpanminus
